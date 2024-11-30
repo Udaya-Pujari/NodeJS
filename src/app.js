f88699dbc2c9fc43e65e9d513dd7111f81342ee3
Udaya-Pujari <udaysp194@gmail.com>
2024-11-30 21:00:07 +0530

    S2E7 completed with GET,PUT,PATCH,POST and delete api creation

--- src/app.js
+++ src/app.js
@@ -9,12 +9,64 @@ app.use(express.json());
 app.post("/signup", async (req, res) => {
   // console.log(req.body);
   const user = new User(req.body);
-
   try {
     await user.save();
     res.send("Added the user Successfully");
   } catch (err) {
     res.status(400).send("Error Saving the user:", err.message);
+  }
+});
+
+//Get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something Went wrong");
+  }
+});
+
+//Feed API-GET/feed -  get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    //to get the the user details just add the empty object, as shown in the below object
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//Delete api:  by id of user
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete({ _id: userId });
+
+    // const user = await User.findByIdAndDelete(userId);
+    res.send("User Deleted Successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//update the user data: By using PATCH API
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data);
+    console.log(user);
+    res.send("user updated successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
   }
 });
 
